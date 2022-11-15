51a79b232fae10147e2872203fa1af025f161110
kureungkureung <terry9611@naver.com>
2022-11-15 22:23:45 +0900

    refactor(LottoTicketsTest): LottoTickets 객체 생성 방식 수정

--- src/test/java/step2step3/lotto/LottoTicketsTest.java
+++ src/test/java/step2step3/lotto/LottoTicketsTest.java
@@ -12,30 +12,12 @@ import java.util.Set;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoTicketsTest {
-
-    @Test
-    void 천원의_배수만큼_로또를_생성한다() {
-        int numberOfTickets = 1;
-        int lottoPrice = 1000;
-        int totalPrice = lottoPrice * numberOfTickets;
-        int manualPurchaseCount = 0;
-        NumbersGenerator numbersGenerator = new NumbersGenerator.Fake(Set.of(1, 2, 3, 4, 5, 6));
-
-        LottoTickets lottoTickets = new LottoTicketsFactory(totalPrice, manualPurchaseCount).lottoTickets(numbersGenerator);
-
-        assertThat(lottoTickets.numberOfTickets()).isOne();
-    }
-
     @Test
     void 수익률_계산기를_생성할_수_있다() {
-        int numberOfTickets = 1;
         int lottoPrice = 1000;
-        int totalPrice = lottoPrice * numberOfTickets;
-        Set<Integer> lottoNumbers = Set.of(1, 2, 3, 4, 5, 6);
-        int manualPurchaseCount = 0;
-        NumbersGenerator numbersGenerator = new NumbersGenerator.Fake(lottoNumbers);
+        NumbersGenerator numbersGenerator = new NumbersGenerator.Fake(Set.of(1, 2, 3, 4, 5, 6));
 
-        LottoTickets lottoTickets = new LottoTicketsFactory(totalPrice, manualPurchaseCount).lottoTickets(numbersGenerator);
+        LottoTickets lottoTickets = new LottoTickets(List.of(LottoTicket.from(numbersGenerator)), lottoPrice);
 
         WinningLotto winningLotto = new WinningLotto(1, LottoTicket.from(numbersGenerator));
         assertThat(lottoTickets.yieldCalculator(winningLotto)).isEqualTo(new YieldCalculator(lottoPrice, List.of(Rank.FIRST)));
@@ -43,14 +25,10 @@ class LottoTicketsTest {
 
     @Test
     void 일치_지표_계산기를_생성할_수_있다() {
-        int numberOfTickets = 1;
         int lottoPrice = 1000;
-        int totalPrice = lottoPrice * numberOfTickets;
-        Set<Integer> lottoNumbers = Set.of(1, 2, 3, 4, 5, 6);
-        int manualPurchaseCount = 0;
-        NumbersGenerator numbersGenerator = new NumbersGenerator.Fake(lottoNumbers);
+        NumbersGenerator numbersGenerator = new NumbersGenerator.Fake(Set.of(1, 2, 3, 4, 5, 6));
 
-        LottoTickets lottoTickets = new LottoTicketsFactory(totalPrice, manualPurchaseCount).lottoTickets(numbersGenerator);
+        LottoTickets lottoTickets = new LottoTickets(List.of(LottoTicket.from(numbersGenerator)), lottoPrice);
 
         WinningLotto winningLotto = new WinningLotto(1, LottoTicket.from(numbersGenerator));
         assertThat(lottoTickets.matchIndicatorCalculator(winningLotto)).isEqualTo(new MatchIndicatorCalculator(List.of(Rank.FIRST)));
